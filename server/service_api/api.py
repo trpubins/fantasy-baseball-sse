b8dd7eb4baba8df3e8c2c90cb4f9c6580b39693e
Tanner Pubins <t.pubins@icloud.com>
2022-01-27 18:46:12 -0700

    Fetching data separated into its own module, fetch.py. Utilizing new PropagatingThread class to propagate exceptions up to the calling thread.

--- server/service_api/api.py
+++ server/service_api/api.py
@@ -4,24 +4,19 @@ a RESTful API.
 """
 
 # Standard imports
-import json
-import os
 import sys
-from threading import Thread
-import time
 
 # 3rd party imports
 from flask import Flask, Response
 from flask_restful import Api, Resource
-import pandas as pd
 
 # Local imports
 sys.path.append('../../')
 from helpers.constants import get_server_addr, get_server_port
 from helpers.paths import up_path
-from helpers.pubsub import MessageAnnouncer, dict_sse, format_sse
-from server.service_api.http_abort import abort_cannot_read_csv, abort_file_not_found
-from server.service_download.download import test
+from helpers.pubsub import MessageAnnouncer
+from helpers.threads import PropagatingThread
+from server.service_api.fetch import fetch_data
 
 
 app = Flask(__name__)
@@ -43,58 +38,19 @@ def stream(announcer: MessageAnnouncer):
     GeneratorExit()
 
 
-def read_csv(announcer: MessageAnnouncer, csv_path: str):
-    """
-    Attempts to read the comma separated values data at the specified path.
-    Publishes the data using the announcer if read successfully.
-
-    :param announcer: The object used for publishing messages.
-    :param csv_path: The absolute path of the csv file.
-    """
-    df = pd.DataFrame()
-    try:
-        # attempt to read the csv data
-        df = pd.read_csv(csv_path)
-    except:
-        abort_cannot_read_csv(csv_path)
-    
-    announcer.finalize_stream()
-    
-    # convert and format the dataframe into an event stream message
-    d = dict_sse(data=df.to_json(), final_stream=True)
-    msg = format_sse(data=json.dumps(d))
-    
-    # publish the message
-    announcer.announce(msg=msg)
-    return
-
-
 def get_resource(csv_path: str) -> Response:
     """
-    Retrieves the csv data from the specified path.
+    Creates an event stream response and fetches the data requested by the client.
 
     :param csv_path: The absolute path of the csv file.
     :return: The streamed Flask Response.
     """
-    # check that the requested resource exists
-    if not os.path.exists(csv_path):
-        abort_file_not_found(csv_path) 
-    
     announcer = MessageAnnouncer()
 
-    # check if file has been modified in the last 12 hours
-    mod_time = os.path.getmtime(csv_path)
-    current_time = time.time()
-    delta_s = current_time - mod_time
-    delta_hr = delta_s / 60 / 60
-
-    if delta_hr > 12:
-        # retrieve latest results from internet
-        thread = Thread(target=test, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
-        thread.start()
-    else:
-        thread = Thread(target=read_csv, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
-        thread.start()
+    # fetch the data using a separate thread
+    thread = PropagatingThread(target=fetch_data, args=(announcer,csv_path,))
+    thread.start()
+    thread.join()
 
     return Response(stream(announcer), mimetype='text/event-stream')
 
---
+++ server/service_api/fetch.py
@@ -0,0 +1,76 @@
+"""
+Fetches the fantasy baseball data for the microservice.
+"""
+
+# Standard imports
+import json
+import os
+import sys
+import time
+
+# 3rd party imports
+import pandas as pd
+
+# Local imports
+sys.path.append('../../')
+from helpers.pubsub import MessageAnnouncer, dict_sse, format_sse
+from helpers.threads import PropagatingThread
+from server.service_api.http_abort import abort_cannot_read_csv, abort_file_not_found
+from server.service_download.download import test
+
+
+def fetch_data(announcer: MessageAnnouncer, csv_path: str):
+    """
+    Fetches the csv data from the specified path, and publishes the data once retrieved.
+
+    :param announcer: The object used for publishing messages.
+    :param csv_path: The absolute path of the csv file.
+    """
+    # check that the requested resource exists
+    if not os.path.exists(csv_path):
+        abort_file_not_found(csv_path)
+
+    # check if file has been modified in the last 12 hours
+    mod_time = os.path.getmtime(csv_path)
+    current_time = time.time()
+    delta_s = current_time - mod_time
+    delta_hr = delta_s / 60 / 60
+
+    if delta_hr > 12:
+        # retrieve latest results from internet
+        thread = PropagatingThread(target=test, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
+        thread.start()
+        thread.join()
+    else:
+        thread = PropagatingThread(target=read_csv, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
+        thread.start()
+        thread.join()
+    
+    return
+    
+
+def read_csv(announcer: MessageAnnouncer, csv_path: str):
+    """
+    Attempts to read the comma separated values data at the specified path.
+    Publishes the data using the announcer if read successfully.
+
+    :param announcer: The object used for publishing messages.
+    :param csv_path: The absolute path of the csv file.
+    """
+    df = pd.DataFrame()
+    try:
+        # attempt to read the csv data
+        df = pd.read_csv(csv_path)
+    except:
+        abort_cannot_read_csv(csv_path)
+    
+    announcer.finalize_stream()
+    
+    # convert and format the dataframe into an event stream message
+    d = dict_sse(data=df.to_json(), final_stream=True)
+    msg = format_sse(data=json.dumps(d))
+    
+    # publish the message
+    announcer.announce(msg=msg)
+    return
+    
