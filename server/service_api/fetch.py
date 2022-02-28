3a87cf9253fe5428a7c07ce60cbbc4492dfdf2fd
Tanner Pubins <t.pubins@icloud.com>
2022-02-27 22:57:57 -0700

    Replace Thread with PropagatingThread class. Moved test function into fetch.py.

--- server/service_api/fetch.py
+++ server/service_api/fetch.py
@@ -6,7 +6,6 @@ Fetches the fantasy baseball data for the microservice.
 import json
 import os
 import sys
-from threading import Thread
 import time
 
 # 3rd party imports
@@ -15,13 +14,16 @@ import pandas as pd
 # Local imports
 sys.path.append('../../')
 from helpers.pubsub import MessageAnnouncer, dict_sse, format_sse
+from helpers.threads import PropagatingThread
 from server.service_api.http_abort import abort_cannot_read_csv, abort_file_not_found
-from server.service_download.download import test
+
+# Constants
+MAX_TIME_BETWEEN_REFRESH = 12  # hrs
 
 
 def fetch_data(announcer: MessageAnnouncer, csv_path: str):
     """
-    Fetches the csv data from the specified path, and publishes the data once retrieved.
+    Fetches the csv data from the specified path using threading, and publishes the data once retrieved.
 
     :param announcer: The object used for publishing messages.
     :param csv_path: The absolute path of the csv file.
@@ -36,16 +38,17 @@ def fetch_data(announcer: MessageAnnouncer, csv_path: str):
     delta_s = current_time - mod_time
     delta_hr = delta_s / 60 / 60
 
-    if delta_hr > 12:
+    if delta_hr > MAX_TIME_BETWEEN_REFRESH:
         # retrieve latest results from internet
-        thread = Thread(target=test, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
+        thread = PropagatingThread(target=test, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
         thread.start()
     else:
-        thread = Thread(target=read_csv, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
+        # read the data on disk
+        thread = PropagatingThread(target=read_csv, args=(announcer,csv_path,))  # threading.Thread class needs an iterable of arguments as the args parameter
         thread.start()
     
     return
-    
+
 
 def read_csv(announcer: MessageAnnouncer, csv_path: str):
     """
@@ -55,7 +58,7 @@ def read_csv(announcer: MessageAnnouncer, csv_path: str):
     :param announcer: The object used for publishing messages.
     :param csv_path: The absolute path of the csv file.
     """
-    df = pd.DataFrame()
+    df = None
     try:
         # attempt to read the csv data
         df = pd.read_csv(csv_path)
@@ -71,3 +74,19 @@ def read_csv(announcer: MessageAnnouncer, csv_path: str):
     # publish the message
     announcer.announce(msg=msg)
     return
+
+
+def test(announcer: MessageAnnouncer, csv_path: str):
+    """
+    A function used to test the SSE stream.
+    """
+    n = 0
+    while n <= 3:
+        if n == 3:
+            announcer.finalize_stream()
+        d = dict_sse(data=f'downloading...{n}', final_stream=announcer.is_final_stream())
+        msg = format_sse(data=json.dumps(d))
+        announcer.announce(msg=msg)
+        n += 1
+        time.sleep(1)
+    return   
